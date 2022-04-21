8951949a6e48961101e7d0a3f59e85c342ada778
Riley1101 <www.riley.s.eileen@gmail.com>
2022-04-21 17:33:13 +0630

    apollo client setup

--- rustic/src/pages/_app.js
+++ rustic/src/pages/_app.js
@@ -1,21 +1,28 @@
 import '../styles/globals.css';
+import { Fragment } from 'react';
 import Layout from '../components/layout/layout';
 import Head from 'next/head';
 import { ScrollToTop } from '../components/scroll';
+import { ApolloProvider } from "@apollo/client";
+import client from '@/query/client';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <Layout>
-            <Head>
-                <meta
-                    name='viewport'
-                    content='width=device-width, initial-scale=1'
-                />
-                <link rel='shortcut icon' href='../favicon.png' />
-            </Head>
-            <Component {...pageProps} />
-            <ScrollToTop />
-        </Layout>
+        <Fragment>
+            <ApolloProvider client={client}>
+                <Layout>
+                    <Head>
+                        <meta
+                            name='viewport'
+                            content='width=device-width, initial-scale=1'
+                        />
+                        <link rel='shortcut icon' href='../favicon.png' />
+                    </Head>
+                    <Component {...pageProps} />
+                    <ScrollToTop />
+                </Layout>
+            </ApolloProvider>
+        </Fragment>
     );
 }
 
---
+++ rustic/src/query/client.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+// apollo client 
+const client = new ApolloClient({
+    uri: "https://countries.trevorblades.com",
+    cache: new InMemoryCache(),
+});
+
+export default client;
